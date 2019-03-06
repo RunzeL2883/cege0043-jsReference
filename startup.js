f870f0d7ed04784616214ec8d764b170aaec25f5
Runze Liu <runze.liu.18@ucl.ac.uk>
2019-03-06 07:22:46 -0500

    debugging 1

--- startup.js
+++ startup.js
@@ -1,10 +1,15 @@
 function trackAndCircle() {
 	addPointLinePoly();
 	trackLocation();
-	getEarthquakes()
+	getEarthquakes();
+	getPort();
+
 }
 function startup() {
 	document.addEventListener('DOMContentLoaded', function() {
 		trackAndCircle ();
 	}, false);
+}
+function loadW3HTML() {
+    w3.includeHTML();
 }
